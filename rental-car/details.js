e883e9c4145d3dfdc409d174308e7e07156e10bc
Lewis Benson <tm.lewis.benson@gmail.com>
2023-12-03 22:05:23 -0500

    build out car rental

--- rental-car/details.js
+++ rental-car/details.js
@@ -1,11 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('rentalForm');
   const totalCostDiv = document.getElementById('totalCost');
+  const carTypeSelect = document.getElementById('carType');
+  const carTypeTitle = document.getElementById('carTypeTitle');
+  const carTypeDetail = document.getElementById('carTypeDetail');
+
+  const carDescriptions = {
+    economy:
+      'Perfect for city trips, our economy cars offer great fuel efficiency and easy handling.',
+    sedan:
+      'Ideal for long drives, our sedans provide comfort and space, ensuring a pleasant journey.',
+    suv: 'Our SUVs are perfect for larger groups or those requiring more space and power on the road.',
+    luxury:
+      'Experience the height of comfort and style with our luxury cars, perfect for making a lasting impression.',
+  };
+
+  carTypeSelect.addEventListener('change', () => {
+    const selectedType = carTypeSelect.value;
+    carTypeTitle.textContent =
+      selectedType.charAt(0).toUpperCase() + selectedType.slice(1);
+    carTypeDetail.textContent = carDescriptions[selectedType];
+  });
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
-
-    const carType = document.getElementById('carType').value;
+    const carType = carTypeSelect.value;
     const rentalDays = parseInt(document.getElementById('rentalDays').value);
     let costPerDay;
 
@@ -26,7 +45,22 @@ document.addEventListener('DOMContentLoaded', () => {
         costPerDay = 0;
     }
 
-    const totalCost = costPerDay * rentalDays;
-    totalCostDiv.innerHTML = `<p>Total Cost for Rental: $${totalCost}</p>`;
+    const baseCost = costPerDay * rentalDays;
+    const taxRate = 0.08; // 8% tax rate
+    const insuranceFee = 15; // daily insurance fee
+    const serviceFee = 20; // one-time service fee
+
+    const taxAmount = baseCost * taxRate;
+    const insuranceCost = insuranceFee * rentalDays;
+    const totalCost = baseCost + taxAmount + insuranceCost + serviceFee;
+
+    totalCostDiv.innerHTML = `
+            <p><strong>Cost Breakdown:</strong></p>
+            <p>Base Cost (${rentalDays} days): $${baseCost.toFixed(2)}</p>
+            <p>Tax (8%): $${taxAmount.toFixed(2)}</p>
+            <p>Insurance Fee (per day): $${insuranceCost.toFixed(2)}</p>
+            <p>Service Fee: $${serviceFee.toFixed(2)}</p>
+            <p><strong>Total Cost: $${totalCost.toFixed(2)}</strong></p>
+        `;
   });
 });
